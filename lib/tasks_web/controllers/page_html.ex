94a3755b62da1cef7bc1845285c776c85a396acc
James Arthur <thruflo@gmail.com>
2025-05-08 21:28:02 +0200

    Generated phoenix app.

---
+++ lib/tasks_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule TasksWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use TasksWeb, :html
+
+  embed_templates "page_html/*"
+end
